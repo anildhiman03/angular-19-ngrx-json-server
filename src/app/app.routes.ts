284a4857a2d186b34ae2ca5530541bb83777b9c8
Anil Kumar <1304712+anildhiman03@users.noreply.github.com>
2025-01-04 10:55:02 +0000

    component, services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { EmpListComponent } from './component/emp/emp-list/emp-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  path: 'employee',
+  component: EmpListComponent,
+];
---
+++ src/app/component/emp/emp-list/emp-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-list',
+  imports: [],
+  templateUrl: './emp-list.component.html',
+  styleUrl: './emp-list.component.css'
+})
+export class EmpListComponent {
+
+}
